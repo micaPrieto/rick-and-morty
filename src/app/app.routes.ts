f1baca53bab07b20ab48aea454c257ddc3147337
Micaela Prieto <micaelaprieto@outlook.com>
2025-04-26 19:40:32 -0300

    Incorporación del componente CharacterDetailPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,10 @@ export const routes: Routes = [
   {
     path: 'characters',
     loadComponent:() => import('./pages/characters-page/characters-page.component'),
-
-    children: [
-      {
-        path: 'id',
-        loadComponent:() => import('./pages/character-profile-page/character-profile-page.component')
-      }
-    ]
-
+  },
+  {
+    path: 'characters/:id',
+    loadComponent:() => import('./pages/character-detail-page/character-detail-page.component')
   },
   {
     path: '**',
---
+++ src/app/pages/character-detail-page/character-detail-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { CharactersService } from '../../services/characters-service';
+
+@Component({
+  selector: 'app-character-detail-page',
+  imports: [],
+  templateUrl: './character-detail-page.component.html'
+})
+export default class CharacterDetailPageComponent implements OnInit{
+
+  private charactersService = inject(CharactersService);
+  private activatedRoute = inject(ActivatedRoute);
+
+  characterSelected = this.charactersService.$characterSelected;
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = params['id'];
+      if (id) {
+        this.charactersService.getCharacterById(+id); // El "+" fuerza a number
+      }
+    });
+  }
+
+
+
+/*
+  private charactersService = inject(CharactersService);
+  private activatedRoute = inject(ActivatedRoute);
+
+  characterSelected = this.charactersService.$characterSelected;
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = params['id'];
+      if (id) {
+        this.charactersService.getCharacterById(+id); // El "+" fuerza a number
+        console.log('funcion ts',this.characterSelected);
+      }
+    });
+  }
+
+*/
+}

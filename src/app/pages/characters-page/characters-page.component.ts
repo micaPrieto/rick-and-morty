edc0924344e97bbe9166f02b03993cb9ce7651e2
Micaela Prieto <micaelaprieto@outlook.com>
2025-04-28 19:55:32 -0300

    Mejora del aspecto visual de las cards

--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -1,6 +1,5 @@
 import { Component} from '@angular/core';
-import { CardsListComponent } from "../../components/cards-list/cards-list.component";
-
+import { CardsListComponent } from '../../components/cards-list/cards-list.component';
 
 
 @Component({

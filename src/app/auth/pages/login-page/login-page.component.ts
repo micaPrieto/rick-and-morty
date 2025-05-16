b8309ad53c4b7c95716afa959dbad68c79bdd1b4
Micaela Prieto <micaelaprieto@outlook.com>
2025-05-16 02:13:31 -0300

    Incorporación de page register-page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+
+  })
 
 }

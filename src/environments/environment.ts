d027b0b44dcd3b4e2270d48499d45f85c84ddce5
Mica Prieto <micaelaprieto@outlook.com>
2025-08-08 13:12:30 -0300

    Update environment.ts : Nueva ruta del backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
 
   //usersBaseUrl : 'https://api-auth-moby.herokuapp.com/api',
-   usersBaseUrl : 'http://localhost:3000/user',
+   usersBaseUrl : 'https://back-rickymorty.onrender.com/user',
 
   charactersBaseUrl:  'https://rickandmortyapi.com/api',
 
   episodesBaseUrl:'https://rickandmortyapi.com/api',
 
-  commentsUrl : 'http://localhost:3000/comments'
+  commentsUrl : 'https://back-rickymorty.onrender.com/comments'
 };
